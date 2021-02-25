71a00b85294a5b262d580014e166c4b0e3b2eb09
maliut <lqn619@163.com>
2021-02-25 22:43:03 +0800

    屏蔽发起会话和加入群聊 暂时没场景，不过先不注释页面，体积应该够

--- src/pages/contact/index.vue
+++ src/pages/contact/index.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="methods">
       <i-cell-group>
-        <i-cell title="发起会话" is-link url="/pages/search/main?type=user"></i-cell>
-        <i-cell title="加入群聊" is-link url="/pages/search/main?type=group"></i-cell>
+<!--        <i-cell title="发起会话" is-link url="/pages/search/main?type=user"></i-cell>-->
+<!--        <i-cell title="加入群聊" is-link url="/pages/search/main?type=group"></i-cell>-->
         <i-cell title="新建群聊" is-link url="/pages/create/main"></i-cell>
         <i-cell title="我的黑名单" is-link url="/pages/blacklist/main"></i-cell>
         <i-cell title="我的群组" is-link url="/pages/groups/main"></i-cell>
